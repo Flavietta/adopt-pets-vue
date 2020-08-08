665d3ed353c5649252b158fe59ed871689d04a11
Flavietta <saidi3mahdi@gmail.com>
2020-08-08 16:02:21 +0100

    Set-up

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import { BootstrapVue } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
+Vue.config.productionTip = false;
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
